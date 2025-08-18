d999f399ba7a54744480145f28ff4a3eaf929424
vegetabledog <2813262902@qq.com>
2025-08-18 17:39:28 +0800

    "翻转链表的两种做法"

---
+++ 206.反转链表.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=206 lang=python3
+#
+# [206] 反转链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        cur = head
+        while cur is not None:
+            nxt = cur.next # 保存一下下一个值
+            cur.next = prev
+            prev = cur
+            cur = nxt
+        return prev
+            
+# @lc code=end
+
